6288d26fc74a32e1690cd20fe1a8f805eb525341
SSARA127 <126672202+SSARA127@users.noreply.github.com>
2023-03-02 16:34:37 +0530

    Delete WalletApplication CURD operation with connection of database directory

--- WalletApplication CURD operation with connection of database/src/main/java/com/walletapp/WalletRepository.java
+++
@@ -1,8 +0,0 @@
-package com.walletapp;
-
-public interface WalletRepository {
-    WalletDto createWallet(WalletDto newWallet);
-    WalletDto getWalletById(Integer  walletId);
-    WalletDto updateWallet(WalletDto wallet);
-    WalletDto deleteWalletById(Integer walletId);
-}
