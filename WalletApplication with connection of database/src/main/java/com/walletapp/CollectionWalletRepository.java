8326614a704aa70db39c97b470b03ae537d4a791
SSARA127 <126672202+SSARA127@users.noreply.github.com>
2023-03-02 16:17:58 +0530

    Delete WalletApplication with connection of database directory

--- WalletApplication with connection of database/src/main/java/com/walletapp/CollectionWalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.walletapp;
-
-public interface CollectionWalletRepository {
-    WalletDto createWallet(WalletDto newWallet);
-    WalletDto getWalletById(Integer  walletId);
-    WalletDto updateWallet(WalletDto wallet);
-    WalletDto deleteWalletById(Integer walletId);
-}
-
--- WalletApplication with connection of database/src/main/java/com/walletapp/WalletException.java
+++
@@ -1,7 +0,0 @@
-package com.walletapp;
-
-public class WalletException extends Exception{
-    public WalletException(String msg){
-        super(msg);
-    }
-}
